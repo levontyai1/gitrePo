cb126e0ab25ecc860b6095e4ce9ab41ebdcb769c
Левон <magaryanlevon@gmail.com>
2022-12-01 18:06:00 +0300

    gitPrint

---
+++ main.py
@@ -0,0 +1,13 @@
+def my_superfunction():
+    print("Много")
+
+
+def main():
+    print("1 принт")
+    print("2 принт")
+    print("3 принт")
+    print("4 принт")
+
+
+if __name__ == '__main__':
+    main()
